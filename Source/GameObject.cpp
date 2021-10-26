e9aa29975f4c81784180083be39003b86f3da41d
Eduardiko <60965207+Eduardiko@users.noreply.github.com>
2021-10-26 13:22:37 +0200

    MathGeoLib Updated

--- Source/GameObject.cpp
+++ Source/GameObject.cpp
@@ -2,15 +2,15 @@
 
 #include "Application.h"
 
-//
-//
-//GameObject::GameObject(Application* app, std::string name, bool active = true) {
-//	this->name = name;
-//	this->active = active;
-//	this->transform = new Transform(app, this);
-//}
-//
-//GameObject::~GameObject()
-//{
-//	delete transform;
-//}
+
+
+GameObject::GameObject(Application* app, std::string name, bool active = true) {
+	this->name = name;
+	this->active = active;
+	this->transform = new Transform(app, this);
+}
+
+GameObject::~GameObject()
+{
+	delete transform;
+}
