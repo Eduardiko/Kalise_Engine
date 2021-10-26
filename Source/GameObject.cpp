8e6c4b789390982ff510ec24a4dcda2d1f8fc795
Eduardiko <60965207+Eduardiko@users.noreply.github.com>
2021-10-26 14:13:22 +0200

    GameObject Compiling :D (set function gives error)

--- Source/GameObject.cpp
+++ Source/GameObject.cpp
@@ -2,11 +2,27 @@
 
 #include "Application.h"
 
+Transform::Transform(Application* app, GameObject* gameObject) {
+	
 
+	//this->position.Set(0.0f, 0.0f, 0.0f);
+	this->position.x = 0.0f;
+	this->position.y = 0.0f;
+	this->position.z = 0.0f;
 
-GameObject::GameObject(Application* app, std::string name, bool active = true) {
+	//this->rotation.Set(0.0f, 0.0f, 0.0f);
+	this->rotation.x = 0.0f;
+	this->rotation.y = 0.0f;
+	this->rotation.z = 0.0f;
+
+	//this->scale.Set(0, 0, 0);
+	this->scale.x = 0.0f;
+	this->scale.y = 0.0f;
+	this->scale.z = 0.0f;
+}
+
+GameObject::GameObject(Application* app, std::string name) {
 	this->name = name;
-	this->active = active;
 	this->transform = new Transform(app, this);
 }
 
--- Source/GameObject.h
+++ Source/GameObject.h
@@ -4,24 +4,23 @@
 #include "Application.h"
 #include "MathGeoLib.h"
 
-class GameObject;
-
-class Transform {
-public:
-	Transform(Application* app, GameObject* gameObject, bool active = true);
-
-	float3 position;
-	float3 rotation;
-	float3 scale;
-};
+class Transform;
 
 class GameObject {
 public:
-	GameObject(Application* app, std::string name, bool active);
+	GameObject(Application* app, std::string name);
 	~GameObject();
 
 	Transform* transform;
 
 	std::string name;
-	bool active;
+};
+
+class Transform {
+public:
+	Transform(Application* app, GameObject* gameObject);
+
+	float3 position;
+	float3 rotation;
+	float3 scale;
 };
